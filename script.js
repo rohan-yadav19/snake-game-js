d0c40697e4cd0a859c15c775ba63ca6e82f02aa3
Rohan Yadav <yadavrohan98040@gmail.com>
2025-04-09 01:19:00 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Game constants and variables
+let direction = { x: 0, y: 0 };
+let foodsound = new Audio("food.mp3");
+let gameOverSound = new Audio("gameover.mp3");
+let moveSound = new Audio("move.mp3");
+let musicSound = new Audio("music.mp3");
+
+//game functions
+
+function main(ctime) {
+  window.requestAnimationFrame(main);
+  console.log(ctime);
+}
+
+//main logic starts here
+window.requestAnimationFrame(main);
